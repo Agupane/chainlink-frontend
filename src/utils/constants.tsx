c3e4477f2697b0a13b39c442487d957143531f0f
apane <agustin.pane@gmail.com>
2019-10-07 14:19:17 -0300

    Updates env.example Changes env var from POLLING_UPDATE_TIME to REACT_APP_POLLING_UPDATE_TIME_IN_SECONDS

--- src/utils/constants.tsx
+++ src/utils/constants.tsx
@@ -3,6 +3,6 @@ export const ORACLE_ADDRESS: string = process.env.REACT_APP_CONTRACT_ADD || '0xA
 export const NETWORK_ID: string = process.env.REACT_APP_NETWORK_ID || '42'
 export const RPC_URL: string =
     process.env.REACT_APP_RPC_URL || 'https://kovan.infura.io/v3/a0cd7db23c5944589e8577229f00e83a'
-export const POLLING_UPDATE_TIME: number = process.env.POLLING_UPDATE_TIME
-    ? parseInt(process.env.POLLING_UPDATE_TIME)
+export const POLLING_UPDATE_TIME: number = process.env.REACT_APP_POLLING_UPDATE_TIME_IN_SECONDS
+    ? parseInt(process.env.REACT_APP_POLLING_UPDATE_TIME_IN_SECONDS) * 1000
     : 1000 * 5 // 5 seconds
